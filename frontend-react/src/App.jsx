29236d3dc477133e32c73e67e4441aef0cb74f48
xanorbkid <corps.bronax@gmail.com>
2025-08-29 00:56:55 +0000

    deployment of Frontend & Backend files to Git

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Django + React</h1>
+      
+    </>
+  )
+}
+
+export default App
